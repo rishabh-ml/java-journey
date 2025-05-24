f1aa9e7a456a8dd0de4a8b5c94590efaa8248a06
rishabh-ml <contact.rishabh.shukla@gmail.com>
2025-05-24 22:39:06 +0530

    Add Java program to demonstrate integer variable usage

---
+++ javafundamentals/Int.java
@@ -0,0 +1,11 @@
+package javafundamentals;
+
+public class Int {
+    public static void main(String[] args) {
+        int p = 2000000;
+        int d = 150000000;
+
+        System.out.println("Population: " + p);
+        System.out.println("Distance: "  + d);
+    }
+}
