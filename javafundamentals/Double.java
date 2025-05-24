722caf7cfb075db748776e0758439bcc84785e26
rishabh-ml <contact.rishabh.shukla@gmail.com>
2025-05-24 22:41:20 +0530

    Add Java program to demonstrate double data type usage

---
+++ javafundamentals/Double.java
@@ -0,0 +1,11 @@
+package javafundamentals;
+
+public class Double {
+    public static void main(String[] args) {
+        double pi = 3.141592653589793;
+        double e = 2.718281828459045;
+
+        System.out.println("Value of Pi: " + pi);
+        System.out.println("Value of e: " + e);
+    }
+}
