dc2b0792e457f86934549afa288fbd57545734c9
rishabh-ml <contact.rishabh.shukla@gmail.com>
2025-05-24 22:51:47 +0530

    Add Java program to demonstrate array creation and access

---
+++ javafundamentals/Array.java
@@ -0,0 +1,12 @@
+package javafundamentals;
+
+public class Array {
+    // Demonstrating how to create an array
+        public static void main(String[] args) {
+            int[] num = {1, 2, 3, 4, 5};
+            String[] arr = {"Geek1", "Geek2", "Geek3"};
+
+            System.out.println("First Number: " + num[0]);
+            System.out.println("Second Fruit: " + arr[1]);
+        }
+}
