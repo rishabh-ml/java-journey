6baac73aea731e5bed36dca48d29cfa90ff0bda2
rishabh-ml <contact.rishabh.shukla@gmail.com>
2025-05-24 22:40:05 +0530

    Add Java program to demonstrate long data type usage

---
+++ javafundamentals/Long.java
@@ -0,0 +1,11 @@
+package javafundamentals;
+
+public class Long {
+    public static void main(String[] args) {
+        long population = 7800000000L; // Population of the world
+        long distance = 384400000L; // Distance from Earth to Moon in meters
+
+        System.out.println("World Population: " + population);
+        System.out.println("Distance to Moon: " + distance + " meters");
+    }
+}
