d156a9dc1c3388437334b9c74273d7ca04c49e83
rishabh-ml <contact.rishabh.shukla@gmail.com>
2025-05-24 22:59:15 +0530

    Add Java program to demonstrate arithmetic operators: addition, subtraction, multiplication, division, and modulus

---
+++ javafundamentals/ArithematicOperators.java
@@ -0,0 +1,28 @@
+package javafundamentals;
+
+public class ArithematicOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        // Addition
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+
+        // Subtraction
+        int difference = a - b;
+        System.out.println("Difference: " + difference);
+
+        // Multiplication
+        int product = a * b;
+        System.out.println("Product: " + product);
+
+        // Division
+        int quotient = a / b;
+        System.out.println("Quotient: " + quotient);
+
+        // Modulus
+        int remainder = a % b;
+        System.out.println("Remainder: " + remainder);
+    }
+}
