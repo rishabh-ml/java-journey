41de76d2c5543d20ca9e217f6f6a53677b2b8944
rishabh-ml <contact.rishabh.shukla@gmail.com>
2025-05-24 22:41:47 +0530

    Add Java program to demonstrate char data type usage

---
+++ javafundamentals/Char.java
@@ -0,0 +1,11 @@
+package javafundamentals;
+
+public class Char {
+    public static void main(String[] args) {
+        char initial = 'A';
+        char grade = 'B';
+
+        System.out.println("Initial: " + initial);
+        System.out.println("Grade: " + grade);
+    }
+}
