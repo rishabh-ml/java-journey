74f414392028b06c560ed38ba8e1535978412796
rishabh-ml <contact.rishabh.shukla@gmail.com>
2025-05-24 22:40:48 +0530

    Add Java program to demonstrate float data type usage

---
+++ javafundamentals/Float.java
@@ -0,0 +1,11 @@
+package javafundamentals;
+
+public class Float {
+    public static void main(String[] args) {
+        float pi = 3.14f;
+        float e = 2.718f;
+
+        System.out.println("Value of Pi: " + pi);
+        System.out.println("Value of e: " + e);
+    }
+}
