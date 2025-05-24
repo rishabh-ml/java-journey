8fa42cfdc155c0a8a25b001b31871c23de83ac51
rishabh-ml <contact.rishabh.shukla@gmail.com>
2025-05-24 22:36:33 +0530

    Add Java program to demonstrate short data type usage

---
+++ javafundamentals/Short.java
@@ -0,0 +1,11 @@
+package javafundamentals;
+
+public class Short {
+    public static void main(String[] args) {
+        short num = 1000;
+        short t = -200;
+
+        System.out.println("Number of Studdents: " + num);
+        System.out.println("Temperature: " + t);
+    }
+}
