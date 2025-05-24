2923fbe872082a924e9108ab4b541b9dacd109e9
rishabh-ml <contact.rishabh.shukla@gmail.com>
2025-05-24 23:21:08 +0530

    Add Java program to demonstrate assignment operators: += and -=

---
+++ javafundamentals/AssignmentOperator.java
@@ -0,0 +1,15 @@
+package javafundamentals;
+
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        // Using assignment operator
+        a += 5; // equivalent to a = a + 5
+        b -= 3; // equivalent to b = b - 3
+
+        System.out.println("Value of a after assignment: " + a);
+        System.out.println("Value of b after assignment: " + b);
+    }
+}
