3b913f8106f10eba84514ca90e65809302d459a3
rishabh-ml <contact.rishabh.shukla@gmail.com>
2025-05-25 16:21:23 +0530

    Add Java program to demonstrate the instanceof operator with class hierarchy

--- javafundamentals/InstanceOfOperator.java
+++ javafundamentals/InstanceOfOperator.java
@@ -2,13 +2,36 @@ package javafundamentals;
 
 public class InstanceOfOperator {
     public static void main(String[] args) {
-        String obj = "Hello, World!";
+        Person obj1 = new Person();
+        Person obj2 = new Boy();
 
-        if (obj instanceof String) {
-            String str = (String) obj;
-            System.out.println("The object is a String: " + str);
-        } else {
-            System.out.println("The object is not a String.");
-        }
+        // As obj is of type person, it is not an
+        // instance of Boy or interface
+        System.out.println("obj1 instanceof Person: "
+                + (obj1 instanceof Person));
+        System.out.println("obj1 instanceof Boy: "
+                + (obj1 instanceof Boy));
+        System.out.println("obj1 instanceof MyInterface: "
+                + (obj1 instanceof MyInterface));
+
+        // Since obj2 is of type boy,
+        // whose parent class is person
+        // and it implements the interface Myinterface
+        // it is instance of all of these classes
+        System.out.println("obj2 instanceof Person: "
+                + (obj2 instanceof Person));
+        System.out.println("obj2 instanceof Boy: "
+                + (obj2 instanceof Boy));
+        System.out.println("obj2 instanceof MyInterface: "
+                + (obj2 instanceof MyInterface));
     }
+}
+
+class Person {
+}
+
+class Boy extends Person implements MyInterface {
+}
+
+interface MyInterface {
 }
